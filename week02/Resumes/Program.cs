52a8f04d7da3c4e63ed9c5df9e7d424decc4154e
ChrisOsunaE <obedosunae@gmail.com>
2025-09-13 11:09:59 -0700

    Add Job and Resume classes with display functionality

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Company A";
+        job1._title = "Software Engineer";
+        job1._startYear = "2020";
+        job1._endYear = "2022";
+
+        Job job2 = new Job();
+        job2._company = "Company B";
+        job2._title = "Senior Software Engineer";
+        job2._startYear = "2022";
+        job2._endYear = "Present";
+
+
+        job1.Display();
+        job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "John Doe";
+        resume.jobs.Add(job1);
+        resume.jobs.Add(job2);
+        resume.Display();
+
     }
 }
