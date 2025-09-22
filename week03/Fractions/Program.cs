f8f3484d53383117f216a8c953b404aa1e359bdf
ChrisOsunaE <obedosunae@gmail.com>
2025-09-22 13:25:27 -0700

    Add Fraction class with basic functionality for fraction operations

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction(6, 8);
+
+        Console.WriteLine("Default Fraction: " + fraction1.GetFractionString());
+        Console.WriteLine("Decimal Value: " + fraction1.GetDecimalValue());
+
+        fraction1.SetTop(2);
+        fraction1.SetBottom(4);
+        Console.WriteLine("Updated Fraction: " + fraction1.GetFractionString());
+        Console.WriteLine("Updated Decimal Value: " + fraction1.GetDecimalValue());
     }
 }
