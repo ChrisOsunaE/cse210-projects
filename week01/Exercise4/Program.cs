f646d09378a6508015cd82b052f54d760a8044c4
Chris Osuna <obedosunae@gmail.com>
2026-06-27 17:01:11 -0700

    Implement number list input, calculations, and sorting in Exercise4 project

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();
+        int number;
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        while (number != 0);
+
+        Console.WriteLine("The sum is: " + numbers.Sum());
+        Console.WriteLine("The average is: " + numbers.Average());
+        Console.WriteLine("The largest number is: " + numbers.Max());
+
+        List<int> positiveNumbers = numbers.Where(n => n > 0).ToList();
+        Console.WriteLine("The smallest positive number is: " + positiveNumbers.Min());
+
+        numbers.Sort();
+        Console.WriteLine("The sorted list of numbers is: ");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
