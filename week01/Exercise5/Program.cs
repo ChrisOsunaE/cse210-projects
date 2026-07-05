5cf6399c458226a67c43981f65dba64f77b7ba38
Chris Osuna <obedosunae@gmail.com>
2026-07-04 21:15:47 -0700

    Refactor code structure by moving method definitions outside of Main in Exercise5 project

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -6,32 +6,33 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
 
-        static void DisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the Program!");
-        }
-
-        static string PromptUserName()
-        {
-            Console.Write("Please enter your name: ");
-            return Console.ReadLine();
-        }
-
-        static int PromptUserNumber()
-        {
-            Console.Write("Please enter your favorite number: ");
-            return int.Parse(Console.ReadLine());
-        }
-
-        static void DisplayResult(string name, int favoriteNumber)
-        {
-            int squareNumber = favoriteNumber * favoriteNumber;
-            Console.WriteLine($"{name}, the square of your number is {squareNumber}");
-        }
-
         DisplayWelcome();
         string userName = PromptUserName();
         int userNumber = PromptUserNumber();
         DisplayResult(userName, userNumber);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void DisplayResult(string name, int favoriteNumber)
+    {
+        int squareNumber = favoriteNumber * favoriteNumber;
+        Console.WriteLine($"{name}, the square of your number is {squareNumber}");
+    }
+
 }
