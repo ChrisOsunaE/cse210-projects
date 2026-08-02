2e78694e854f5d04bfba25f03731a03be433edc0
Chris Osuna <obedosunae@gmail.com>
2026-08-02 01:35:37 -0700

    Add Assignment, MathAssignment, and WritingAssignment classes with basic functionalities

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Peter Parker", "Web Development");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment a2 = new MathAssignment("Clark Kent", "Algebra", "Section 5.2", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment a3 = new WritingAssignment("Bruce Wayne", "Literature", "The Dark Knight");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
